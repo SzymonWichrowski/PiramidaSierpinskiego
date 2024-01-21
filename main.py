561c7af6fcff0162ca7eee232fcb139725267085
SzymonWichrowski <szymonwichrowski99@gmail.com>
2024-01-21 23:08:07 +0100

    update - dodanie tekstury

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ whichSpin = 0
 
 light_position = [0.0, 0.0, 3.0, 1.0]
 
+texture_surface = pygame.image.load("tekstura.jpg")
+texture_data = pygame.image.tostring(texture_surface, "RGB", 1)
+show_texture = False
+
 def shutdown():
     pygame.quit()
     sys.exit()
@@ -110,6 +114,74 @@ def axes():
 
     glEnd()
 
+def load_texture():
+    glEnable(GL_TEXTURE_2D)
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, texture_surface.get_width(), texture_surface.get_height(),
+                 0, GL_RGB, GL_UNSIGNED_BYTE, texture_data)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+
+def draw_textured_triangle(wierz1, wierz2, wierz3):
+    glEnable(GL_TEXTURE_2D)
+    glBegin(GL_TRIANGLES)
+    glTexCoord2f(0.0, 0.0)
+    glVertex3f(wierz1[0], wierz1[1], wierz1[2])
+
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(wierz2[0], wierz2[1], wierz2[2])
+
+    glTexCoord2f(0.5, 1.0)
+    glVertex3f(wierz3[0], wierz3[1], wierz3[2])
+    glEnd()
+    glDisable(GL_TEXTURE_2D)
+
+def reg_tetrahedron_with_texture(wierz1, wierz2, wierz3, wierz4):
+    draw_textured_triangle(wierz1, wierz2, wierz3)
+    draw_textured_triangle(wierz1, wierz3, wierz4)
+    draw_textured_triangle(wierz2, wierz3, wierz4)
+    draw_textured_triangle(wierz1, wierz2, wierz4)
+
+def division_with_texture(v1, v2, v3, v4, n):
+    v12 = [0.0, 0.0, 0.0]
+    v23 = [0.0, 0.0, 0.0]
+    v13 = [0.0, 0.0, 0.0]
+    v14 = [0.0, 0.0, 0.0]
+    v24 = [0.0, 0.0, 0.0]
+    v34 = [0.0, 0.0, 0.0]
+    if n > 0:
+        # wierzcholek pomiedzy wierz1 i wierz2
+        v12[0] = (v1[0] + v2[0]) / 2
+        v12[1] = (v1[1] + v2[1]) / 2
+        v12[2] = (v1[2] + v2[2]) / 2
+        # wierzcholek pomiedzy wierz2 i wierz3
+        v23[0] = (v3[0] + v2[0]) / 2
+        v23[1] = (v3[1] + v2[1]) / 2
+        v23[2] = (v3[2] + v2[2]) / 2
+        # wierzcholek pomiedzy wierz1 i wierz3
+        v13[0] = (v1[0] + v3[0]) / 2
+        v13[1] = (v1[1] + v3[1]) / 2
+        v13[2] = (v1[2] + v3[2]) / 2
+        # wierzcholek pomiedzy wierz1 i wierz4
+        v14[0] = (v1[0] + v4[0]) / 2
+        v14[1] = (v1[1] + v4[1]) / 2
+        v14[2] = (v1[2] + v4[2]) / 2
+        # wierzcholek pomiedzy wierz2 i wierz4
+        v24[0] = (v4[0] + v2[0]) / 2
+        v24[1] = (v4[1] + v2[1]) / 2
+        v24[2] = (v4[2] + v2[2]) / 2
+        # wierzcholek pomiedzy wierz3 i wierz4
+        v34[0] = (v4[0] + v3[0]) / 2
+        v34[1] = (v4[1] + v3[1]) / 2
+        v34[2] = (v4[2] + v3[2]) / 2
+
+        division_with_texture(v1, v12, v13, v14, n - 1)
+        division_with_texture(v12, v2, v23, v24, n - 1)
+        division_with_texture(v13, v23, v3, v34, n - 1)
+        division_with_texture(v14, v24, v34, v4, n - 1)
+
+    else:
+        reg_tetrahedron_with_texture(v1, v2, v3, v4)
+
 def render(N):
     global vertices
     global rotate_speed
@@ -124,7 +196,11 @@ def render(N):
     elif whichSpin == 2:
         spinX(time_passed * rotate_speed)
 
-    division(vertices[0], vertices[1], vertices[2], vertices[3], N)
+    if show_texture:
+        load_texture()
+        division_with_texture(vertices[0], vertices[1], vertices[2], vertices[3], N)
+    else:
+        division(vertices[0], vertices[1], vertices[2], vertices[3], N)
 
     glFlush()
 
@@ -168,7 +244,8 @@ def main():
         else:
             ok = 1
 
-    global whichSpin, vertices
+    global whichSpin
+    global show_texture
 
     pygame.init()
     display = (800, 600)
@@ -207,6 +284,8 @@ def main():
                     whichSpin = 1
                 if event.key == pygame.K_x:
                     whichSpin = 2
+                if event.key == pygame.K_t:
+                    show_texture = not show_texture
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glEnable(GL_DEPTH_TEST)
