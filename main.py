730b122708c2dae590354ca88cba76f8bdf48e95
SzymonWichrowski <szymonwichrowski99@gmail.com>
2024-01-21 17:18:42 +0100

    big update - PYGAME

--- main.py
+++ main.py
@@ -1,40 +1,95 @@
-import math
-import sys
-
-from glfw.GLFW import *
-
+import pygame
 from OpenGL.GL import *
+from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-def startup():
-    glClearColor(0.0, 0.0, 0.0,  1.0)
-    update_viewport(None, 1000, 1000)
-    glEnable(GL_DEPTH_TEST)
+import math
+
+vertices = [
+    [-math.sqrt(3) / 2, -0.5, 0.0],  # A
+    [math.sqrt(3) / 2, -0.5, 0.0],   # B
+    [0.0, 1.0, 0.0],                 # C
+    [0.0, 0.0, math.sqrt(2)]        # D
+]
+
+rotate_speed = 0.05
+whichSpin = 0
 
 def shutdown():
-    pass
+    pygame.quit()
+    sys.exit()
 
-def update_viewport(window, width, height):
-    if height == 0:
-        height = 1
-    if width == 0:
-        width = 1
-    aspectRatio = width / height
+def eq_triangle(wierz1, wierz2, wierz3):
+    glBegin(GL_TRIANGLES)
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex3f(wierz1[0], wierz1[1], wierz1[2])
+    glColor3f(1.0, 0.0, 1.0)
+    glVertex3f(wierz2[0], wierz2[1], wierz2[2])
+    glColor3f(1.0, 1.0, 0.0)
+    glVertex3f(wierz3[0], wierz3[1], wierz3[2])
+    glEnd()
 
-    glMatrixMode(GL_PROJECTION)
-    glViewport(0,0,width, height)
-    glLoadIdentity()
+def reg_tetrahedron(wierz1, wierz2, wierz3, wierz4):
+    # glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    # ABC
+    eq_triangle(wierz1, wierz2, wierz3)
+    # ACD
+    eq_triangle(wierz1, wierz3, wierz4)
+    # BCD
+    eq_triangle(wierz2, wierz3, wierz4)
+    # ABD
+    eq_triangle(wierz1, wierz2, wierz4)
+    # glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
 
-    zakres = 1.5
+def division(v1, v2, v3, v4, n):
+    v12 = [0.0, 0.0, 0.0]
+    v23 = [0.0, 0.0, 0.0]
+    v13 = [0.0, 0.0, 0.0]
+    v14 = [0.0, 0.0, 0.0]
+    v24 = [0.0, 0.0, 0.0]
+    v34 = [0.0, 0.0, 0.0]
+    if n > 0:
+        # wierzcholek pomiedzy wierz1 i wierz2
+        v12[0] = (v1[0] + v2[0]) / 2
+        v12[1] = (v1[1] + v2[1]) / 2
+        v12[2] = (v1[2] + v2[2]) / 2
+        # wierzcholek pomiedzy wierz2 i wierz3
+        v23[0] = (v3[0] + v2[0]) / 2
+        v23[1] = (v3[1] + v2[1]) / 2
+        v23[2] = (v3[2] + v2[2]) / 2
+        # wierzcholek pomiedzy wierz1 i wierz3
+        v13[0] = (v1[0] + v3[0]) / 2
+        v13[1] = (v1[1] + v3[1]) / 2
+        v13[2] = (v1[2] + v3[2]) / 2
+        # wierzcholek pomiedzy wierz1 i wierz4
+        v14[0] = (v1[0] + v4[0]) / 2
+        v14[1] = (v1[1] + v4[1]) / 2
+        v14[2] = (v1[2] + v4[2]) / 2
+        # wierzcholek pomiedzy wierz2 i wierz4
+        v24[0] = (v4[0] + v2[0]) / 2
+        v24[1] = (v4[1] + v2[1]) / 2
+        v24[2] = (v4[2] + v2[2]) / 2
+        # wierzcholek pomiedzy wierz3 i wierz4
+        v34[0] = (v4[0] + v3[0]) / 2
+        v34[1] = (v4[1] + v3[1]) / 2
+        v34[2] = (v4[2] + v3[2]) / 2
 
-    if width <= height:
-        glOrtho(-zakres, zakres, -zakres / aspectRatio, zakres / aspectRatio, zakres, -zakres)
+        division(v1, v12, v13, v14, n - 1)
+        division(v12, v2, v23, v24, n - 1)
+        division(v13, v23, v3, v34, n - 1)
+        division(v14, v24, v34, v4, n - 1)
+
     else:
-        glOrtho(-zakres * aspectRatio, zakres * aspectRatio, -zakres, zakres, zakres, -zakres)
+        reg_tetrahedron(v1, v2, v3, v4)
 
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
+def spinZ(spin_angle):
+    glRotatef(spin_angle, 0.0, 0.0, 1.0)
 
+def spinX(spin_angle):
+    glRotatef(spin_angle, 1.0, 0.0, 0.0)
+
+def spinY(spin_angle):
+    glRotatef(spin_angle, 0.0, 1.0, 0.0)
 
 def axes():
     glBegin(GL_LINES)
@@ -53,127 +108,79 @@ def axes():
 
     glEnd()
 
-def spin(kat_obrotu):
-    glRotatef(kat_obrotu, 0.0, 0.0, 1.0)
+def render(N):
+    global vertices
+    global rotate_speed
+    global whichSpin
 
-def trojkat_rownoboczny(wierz1, wierz2, wierz3):
-    glBegin(GL_TRIANGLES)
-    glColor(0.0, 0.0, 1.0)
-    glVertex3f(wierz1[0], wierz1[1], wierz1[2])
-    glColor(1.0, 0.0, 1.0)
-    glVertex3f(wierz2[0], wierz2[1], wierz2[2])
-    glColor(1.0, 1.0, 0.0)
-    glVertex3f(wierz3[0], wierz3[1], wierz3[2])
-    glEnd()
-
-def czworoscian_foremny(wierz1, wierz2, wierz3, wierz4):
-    #glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
-    #ABC
-    trojkat_rownoboczny(wierz1, wierz2, wierz3)
-    #ACD
-    trojkat_rownoboczny(wierz1, wierz3, wierz4)
-    #BCD
-    trojkat_rownoboczny(wierz2, wierz3, wierz4)
-    #ABD
-    trojkat_rownoboczny(wierz1, wierz2, wierz4)
-    #glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
-
-def podzial(wierz1, wierz2, wierz3, wierz4, n):
-    wierz12 = [0.0, 0.0, 0.0]
-    wierz23 = [0.0, 0.0, 0.0]
-    wierz13 = [0.0, 0.0, 0.0]
-    wierz14 = [0.0, 0.0, 0.0]
-    wierz24 = [0.0, 0.0, 0.0]
-    wierz34 = [0.0, 0.0, 0.0]
-    if (n > 0):
-        #wierzcholek pomiedzy wierz1 i wierz2
-        wierz12[0] = (wierz1[0] + wierz2[0]) / 2
-        wierz12[1] = (wierz1[1] + wierz2[1]) / 2
-        wierz12[2] = (wierz1[2] + wierz2[2]) / 2
-        #wierzcholek pomiedzy wierz2 i wierz3
-        wierz23[0] = (wierz3[0] + wierz2[0]) / 2
-        wierz23[1] = (wierz3[1] + wierz2[1]) / 2
-        wierz23[2] = (wierz3[2] + wierz2[2]) / 2
-        # wierzcholek pomiedzy wierz1 i wierz3
-        wierz13[0] = (wierz1[0] + wierz3[0]) / 2
-        wierz13[1] = (wierz1[1] + wierz3[1]) / 2
-        wierz13[2] = (wierz1[2] + wierz3[2]) / 2
-        # wierzcholek pomiedzy wierz1 i wierz4
-        wierz14[0] = (wierz1[0] + wierz4[0]) / 2
-        wierz14[1] = (wierz1[1] + wierz4[1]) / 2
-        wierz14[2] = (wierz1[2] + wierz4[2]) / 2
-        # wierzcholek pomiedzy wierz2 i wierz4
-        wierz24[0] = (wierz4[0] + wierz2[0]) / 2
-        wierz24[1] = (wierz4[1] + wierz2[1]) / 2
-        wierz24[2] = (wierz4[2] + wierz2[2]) / 2
-        # wierzcholek pomiedzy wierz3 i wierz4
-        wierz34[0] = (wierz4[0] + wierz3[0]) / 2
-        wierz34[1] = (wierz4[1] + wierz3[1]) / 2
-        wierz34[2] = (wierz4[2] + wierz3[2]) / 2
-
-        podzial(wierz1, wierz12, wierz13, wierz14, n-1)
-        podzial(wierz12, wierz2, wierz23, wierz24, n-1)
-        podzial(wierz13, wierz23, wierz3, wierz34, n-1)
-        podzial(wierz14, wierz24, wierz34, wierz4, n-1)
-
-    else:
-        czworoscian_foremny(wierz1, wierz2, wierz3, wierz4)
-
-
-def render(time, N):
-
-    wierzcholki = [
-        [-math.sqrt(3) / 2, -0.5, 0.0],     # A
-        [math.sqrt(3) / 2, -0.5, 0.0],      # B
-        [0.0, 1.0, 0.0],                    # C
-        [0.0, 0.0, -math.sqrt(2)]           # D
-    ]
-    glClear(GL_COLOR_BUFFER_BIT)
-    glClear(GL_DEPTH_BUFFER_BIT)
-
-    glLoadIdentity()
     axes()
+    time_passed = pygame.time.get_ticks()
+    if whichSpin == 0:
+        spinZ(time_passed * rotate_speed)
+    elif whichSpin == 1:
+        spinY(time_passed * rotate_speed)
+    elif whichSpin == 2:
+        spinX(time_passed * rotate_speed)
 
-    glLoadIdentity()
-    spin(time * 180 / 3.1415)
-
-    podzial(wierzcholki[0], wierzcholki[1], wierzcholki[2], wierzcholki[3], N)
+    division(vertices[0], vertices[1], vertices[2], vertices[3], N)
 
     glFlush()
 
 def main():
     ok = 0
-    print("Algorytm konstrukcji Piramidy Sierpinskiego przyjmuje jako parametr liczbe podzialow czworoscianu foremnego na mniejsze czworosciany foremne")
-    while(ok == 0):
+    print(
+        "Algorytm konstrukcji Piramidy Sierpinskiego przyjmuje jako parametr liczbe podzialow czworoscianu foremnego na mniejsze czworosciany foremne")
+    while ok == 0:
         print("Podaj liczbe podzialow:")
         N = int(input())
-        if(N < 0):
+        if N < 0:
             print("Nieprawidlowa liczba podzialow. Prosze sprobowac ponownie!")
         else:
             ok = 1
 
-    if not glfwInit():
-        sys.exit(-1)
+    global whichSpin
 
-    window = glfwCreateWindow(1000, 1000, __file__, None, None)
-    if not window:
-        glfwTerminate()
-        sys.exit(-1)
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, pygame.OPENGL | pygame.DOUBLEBUF)
 
-    glfwMakeContextCurrent(window)
-    glfwSetFramebufferSizeCallback(window, update_viewport)
-    glfwSwapInterval(1)
+    glEnable(GL_DEPTH_TEST)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glMatrixMode(GL_MODELVIEW)
+    gluLookAt(0, 0, 4,  #eye
+              0, 0, 0,  #center
+              0, 1, 0)  #up
 
-    startup()
-    while not glfwWindowShouldClose(window):
-        render(glfwGetTime(), N)
-        glfwSwapBuffers(window)
-        glfwWaitEvents()
-    shutdown()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                shutdown()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    shutdown()
+                if event.key == pygame.K_UP:
+                    glTranslatef(0, 0.2, 0)
+                if event.key == pygame.K_DOWN:
+                    glTranslatef(0, -0.2, 0)
+                if event.key == pygame.K_LEFT:
+                    glTranslatef(-0.2, 0, 0)
+                if event.key == pygame.K_RIGHT:
+                    glTranslatef(0.2, 0, 0)
+                if event.key == pygame.K_z:
+                    whichSpin = 0
+                if event.key == pygame.K_y:
+                    whichSpin = 1
+                if event.key == pygame.K_x:
+                    whichSpin = 2
 
-    glfwTerminate()
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glEnable(GL_DEPTH_TEST)
+        glPushMatrix()
+        render(N)
+        glPopMatrix()
 
+        pygame.display.flip()
+        pygame.time.wait(10)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
